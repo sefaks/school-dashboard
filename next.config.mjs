ad1fea1fc017464aadfcc95204134ca0a17432e3
sefaks <sefakse11@gmail.com>
2025-02-24 14:48:26 +0300

    Revert "tryimg"
    This reverts commit d5ff86cd4129378f83f37f51cade5a2e8df7c985.

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "images.pexels.com" }]
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // Azure Static Web Apps için gerekli ayarlar
-  trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      }
-    ]
-  }
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [{hostname: "images.pexels.com"}]
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    }
+  };
+  
+  export default nextConfig;
