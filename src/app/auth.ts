869a1062db8a5f6fc22f554aa5df6ea63792f201
sefakse17 <sefa.kose@ordinatrum.com.tr>
2025-09-28 16:22:45 +0300

    dev update

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -23,7 +23,7 @@ export const authOptions: NextAuthOptions = {
           const { email, password, name, surname, isRegister } = credentials as unknown as { email: string; password: string; name?: string; surname?:string, isRegister?: boolean };
 
           if(isRegister){
-            const register_response = await fetch("https://arfbackend-h3g2bdftbxdffqcy.westeurope-01.azurewebsites.net/auth/teacher/register", {
+            const register_response = await fetch("http://127.0.0.1:8000/auth/teacher/register", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({ email, password, name, surname }),
@@ -49,7 +49,7 @@ export const authOptions: NextAuthOptions = {
       
           if (teacher) {
             // Öğretmen bulunduysa, öğretmen girişi için API'yi çağırıyoruz
-            let backendResponse = await fetch("https://arfbackend-h3g2bdftbxdffqcy.westeurope-01.azurewebsites.net/auth/teacher/login", {
+            let backendResponse = await fetch("http://127.0.0.1:8000/auth/teacher/login", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({ email, password }),
@@ -76,7 +76,7 @@ export const authOptions: NextAuthOptions = {
             }
           } else if (admin) {
             // Eğer admin bulunduysa, admin girişi için API'yi çağırıyoruz
-            let backendResponse = await fetch("https://arfbackend-h3g2bdftbxdffqcy.westeurope-01.azurewebsites.net/auth/institution_admin/login", {
+            let backendResponse = await fetch("http://127.0.0.1:8000/auth/institution_admin/login", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({ email, password }),
--- src/lib/apiClient.tsx
+++ src/lib/apiClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://arfbackend-h3g2bdftbxdffqcy.westeurope-01.azurewebsites.net", // Backend API URL
+  baseURL: "http://127.0.0.1:8000", // Backend API URL
   headers: {
     "Content-Type": "application/json",
   },
